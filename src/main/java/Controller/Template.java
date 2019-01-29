a884edbd51752648b507e0a85704b1f312180aa3
Kamil Brodziak <kamil98brodziak@gmail.com>
2019-01-29 22:14:37 +0100

    Add incomplete studentpanel twig

--- src/main/java/Controller/Template.java
+++ src/main/java/Controller/Template.java
@@ -7,40 +7,20 @@ import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.*;
 
 public class Template implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        // generate a lucky number
-        int luckyNumber = new Random().nextInt(100);
-
-        // create a sample hashmap
-        Map<String, String> usersPass = new HashMap<>();
-        usersPass.put("user", "user");
-        usersPass.put("admin", "admin");
-        usersPass.put("haslo", "maslo");
-
-        List<ArrayList<String>> userPasses = new ArrayList<>();
-        ArrayList<String> userPass = new ArrayList<>();
-        userPass.add("test");
-        userPass.add("test2");
-        userPasses.add(userPass);
-
-        // client's address
-        String userAgent = httpExchange.getRequestHeaders().getFirst("User-agent");
-
         // get a template file
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/example.twig");
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("public_html/studentPanel.twig");
+
 
         // create a model that will be passed to a template
         JtwigModel model = JtwigModel.newModel();
+        model.with("coins", "Peter");
+        model.with("level", "Pater");
 
-        // fill the model with values
-        model.with("client", userAgent);
-        model.with("lucky_number", luckyNumber);
-        model.with("users_pass", userPasses);
 
         // render a template to a string
         String response = template.render(model);
@@ -52,4 +32,5 @@ public class Template implements HttpHandler {
         os.close();
 
     }
+
 }
