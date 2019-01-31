e86fcdcd1d52b1e9ec4c27bc8d16996f81bea91e
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-01-31 22:23:34 +0100

    add styles, lorem ipsum to Main.twig  section

--- src/main/java/Controller/QuestManager.java
+++ src/main/java/Controller/QuestManager.java
@@ -15,7 +15,7 @@ public class QuestManager implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
-        Map<String, String> map = new HashMap<>();
+        Map<String, Object> map = new HashMap<>();
 
         try {
             menuService.loadPageWithMenu(httpExchange, "questManager", map, Accounts.STUDENT);
